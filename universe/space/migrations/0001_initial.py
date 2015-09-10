b54b7d3bcef08cd78cf146d03faed619372e5067
Pete Coward <peter@catalpa.io>
2015-09-10 10:16:11 +0900

    added planet model and initial migration

---
+++ universe/space/migrations/0001_initial.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Planet',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=12)),
+                ('mass', models.IntegerField()),
+                ('distance_from_sun', models.IntegerField()),
+            ],
+        ),
+    ]
--- universe/space/models.py
+++ universe/space/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Planet(models.Model):
+    name = models.CharField(max_length=12, blank=False, null=False)
+    mass = models.IntegerField()
+    distance_from_sun = models.IntegerField()
